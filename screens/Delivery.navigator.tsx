f032b96c63027f44bc760b59634a8af83e3e5dd2
Daniel Andersson <d.andersson86@icloud.com>
2022-05-09 10:56:54 +0200

    Add navigation for deliveries form to enable creating new deliveries.

--- screens/Delivery.navigator.tsx
+++ screens/Delivery.navigator.tsx
@@ -12,10 +12,13 @@ import * as Style from '../assets/styles';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import coverIMG from '../assets/img/NutsAndBolts-6.jpg';
+import { Router } from '@react-navigation/native';
 
 type DeliveriesPropsType = {
     deliveries: Deliveries;
     setDeliveries: object;
+    navigation: NativeStackNavigatorProps;
+    route: Router<any, any>;
 };
 
 const Stack: NativeStackNavigatorProps = createStackNavigator();
@@ -63,6 +66,14 @@ export const DeliveryNavigator: (props: DeliveriesPropsType) => JSX.Element = (
 
                     <Stack.Screen name='Inleveransspecifikation'>
                         {(props: React.PropsWithChildren<object>) => (
+                            <DeliveryItem {...props} />
+                        )}
+                    </Stack.Screen>
+
+                    <Stack.Screen name='Inleverasformulär'>
+                        {(props: React.PropsWithChildren<object>) => (
+                            // TODO Här ska en ny komponent importeras och användas. Komponenten/screen ska vara ett formulär som skapar en ny inleverans enligt kraven.
+                            // TODO: När en ny komponent/screen har skapats, byt ut DeliveryItem mot den.
                             <DeliveryItem {...props} />
                         )}
                     </Stack.Screen>
