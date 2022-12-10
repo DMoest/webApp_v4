7b04b074204409dc1914fac0b58b539036b362a4
Daniel Andersson <d.andersson86@icloud.com>
2022-12-10 15:07:19 +0100

    Update models.Auth: remove unnecessary imports and add a login function.

--- models/Auth.tsx
+++ models/Auth.tsx
@@ -1,12 +1,9 @@
 /**
- * Module imports.
- */
-import config from '../config/config.json';
-import { RequestErrorHandler } from '../components/Utils/ErrorHandler';
-
-/**
  * Function to check if logged in.
  */
 export async function loggedIn() {
     return false;
+}
+
+export async function login(username: string, password: string) {
 }
