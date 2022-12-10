9cd9a9b365ffdb2c55ab8256fe68dbcbe8e6fe74
Daniel Andersson <d.andersson86@icloud.com>
2022-12-10 15:03:31 +0100

    Update AuthProvider, add state for user, add login/logout functions to set user-state.

--- context/Auth.provider.tsx
+++ context/Auth.provider.tsx
@@ -1,14 +1,15 @@
 /**
  * Module imports.
  */
-import React, { createContext, useState } from 'react';
+import React, {createContext, useState} from 'react';
 import * as AuthInterfaces from '../interfaces/Auth';
-import * as AuthModel from '../models/Auth';
 
 /**
  * Authentication context types.
  */
 type AuthContextType = {
+    user: AuthInterfaces.User;
+    setUser: (user: AuthInterfaces.User) => void;
     isLoggedIn: boolean;
     setIsLoggedIn: (authIndicator: boolean) => void;
     login: (username: string, password: string) => void;
@@ -19,12 +20,21 @@ type AuthContextType = {
  * Authentication context.
  */
 const AuthContext = createContext<AuthContextType>({
+    user: undefined,
+    setUser(user: AuthInterfaces.User | null): void {
+    },
+
     isLoggedIn: false,
-    setIsLoggedIn: (email) => {
+    setIsLoggedIn: () => {
         // What to do?
     },
-    login: (username, password) => {},
-    logout: () => {},
+    login: (username: string, password: string) => {
+        // Login user with AuthModel.login()
+        // setUser(username);
+    },
+    logout: () => {
+        // Logout user with AuthModel.logout()
+    },
 });
 
 /**
@@ -33,22 +43,27 @@ const AuthContext = createContext<AuthContextType>({
  * @param children
  * @constructor
  */
-export const AuthProvider: React.FC = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+export const AuthProvider: React.FC = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState('Daniel');
+    const [user, setUser] = useState('Daniel');
 
     return (
         <AuthContext.Provider
             value={{
+                user,
+                setUser: (user: AuthInterfaces.User) => {
+                },
                 isLoggedIn,
                 setIsLoggedIn,
                 login: (email, password) => {
-                    // Communicate with backend
-                    // store token in SecureStore
-
+                    // Communicate with backend and store token in SecureStore
                     setIsLoggedIn('Daniel@example.com');
+                    setUser('Daniel');
+                    // SecureStore.setItemAsync('user', 'Daniel@example.com');
                 },
                 logout: () => {
                     setIsLoggedIn(null);
+                    setUser(null);
                 },
             }}>
             {children}
