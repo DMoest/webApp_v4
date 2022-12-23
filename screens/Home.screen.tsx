13059bed12e39751a6bbea61ae0ca1590f07d972
Daniel Andersson <d.andersson86@icloud.com>
2022-12-23 10:01:37 +0100

    Update Home screen.

--- screens/Home.screen.tsx
+++ screens/Home.screen.tsx
@@ -1,6 +1,3 @@
-/**
- * Module imports.
- */
 import React from 'react';
 // eslint-disable-next-line import/namespace
 import {SafeAreaView, Text, View} from 'react-native';
@@ -9,7 +6,6 @@ import {CoverImage} from '../components/Utils/CoverImage';
 // @ts-ignore
 import coverIMG from '../assets/img/NutsAndBolts-4.jpg';
 import * as Style from '../assets/styles';
-import {useAppContext} from "../context/App.provider";
 
 /**
  * Home screen/view.
@@ -17,16 +13,14 @@ import {useAppContext} from "../context/App.provider";
  * @constructor
  */
 export const Home: React.FC = () => {
-    const appContext = useAppContext();
-
     return (
         <SafeAreaView style={Style.Base.mainContainer}>
-            {CoverImage({headerText: 'Infinity', image: coverIMG})}
+            {CoverImage({ headerText: 'Infinity', image: coverIMG })}
 
             <View
                 style={[
                     Style.Container.containers,
-                    {paddingHorizontal: Style.Typography.whiteSpace.X075},
+                    { paddingHorizontal: Style.Typography.whiteSpace.X075 },
                 ]}>
                 <Text style={Style.Typography.paragraph}>
                     Välkommen till vår fiktiva mobila lagerapp. Idag lagrar vi
