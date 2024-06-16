db2405c77caa70679f8557f6f2ac30da97a2e192
Daniel Andersson <d.andersson86@icloud.com>
2024-06-16 17:53:13 +0200

    Update Order.ts module, add interface OrderItemProps

--- interfaces/Order.ts
+++ interfaces/Order.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-unresolved
+import {NativeStackNavigatorProps} from 'react-native-screens/lib/typescript/native-stack/types';
+
 /**
  * Order interface.
  */
@@ -9,7 +12,7 @@ export interface Order {
     city: string;
     country: string;
     status: string;
-    status_id: string;
+    status_id: number;
     api_key: string;
     order_items: OrderItem[] | [];
 }
@@ -18,13 +21,26 @@ export interface Order {
  * Order Item interface.
  */
 export interface OrderItem {
-    "product_id": number,
-    "amount": number,
-    "article_number": string,
-    "name": string,
-    "description": string,
-    "specifiers": string,
-    "stock": number,
-    "location": string,
-    "price": number
+    product_id: number;
+    amount: number;
+    article_number: string;
+    name: string;
+    description: string;
+    specifiers: string;
+    stock: number;
+    location: string;
+    price: number;
+}
+
+/**
+ * Order Item Props interface.
+ * Used for passing props to OrderItem component.
+ */
+export interface OrderItemProps {
+    route: {
+        params: {
+            item: Order;
+        };
+    };
+    navigation: NativeStackNavigatorProps;
 }
