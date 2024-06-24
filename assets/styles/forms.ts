293220bde1794058de48e0dba8ab1d6e6a2adae8
Daniel Andersson <d.andersson86@icloud.com>
2024-06-24 08:16:32 +0200

    Update the forms.ts styles module

--- assets/styles/forms.ts
+++ assets/styles/forms.ts
@@ -3,6 +3,7 @@
  */
 import * as Color from './colors';
 import * as Container from './containers';
+import * as Typography from './typography';
 
 /**
  *
@@ -10,9 +11,9 @@ import * as Container from './containers';
 export const textInputField = {
     width: '100%',
     height: 'auto',
-    paddingHorizontal: Container.whiteSpace.X050,
-    paddingVertical: Container.whiteSpace.X050,
-    marginBottom: Container.whiteSpace.X010,
+    paddingHorizontal: Typography.whiteSpace[50],
+    paddingVertical: Typography.whiteSpace[50],
+    marginBottom: Typography.whiteSpace[10],
     backgroundColor: Color.grayScale.gray4,
     color: Color.text.dark,
     borderRadius: Container.borderRadius.button,
@@ -23,8 +24,8 @@ export const textInputField = {
 export const labelInputField = {
     width: '100%',
     height: 30,
-    paddingHorizontal: Container.whiteSpace.X025,
-    paddingVertical: Container.whiteSpace.X050,
+    paddingHorizontal: Typography.whiteSpace[25],
+    paddingVertical: Typography.whiteSpace[50],
 };
 
 export const pickers = {
@@ -40,3 +41,19 @@ export const pickers = {
 //     paddingHorizontal: 0,
 //     paddingVertical: 0,
 // };
+
+export const TabBarStyles = {
+    tabBar: {
+        width: '100%',
+        height: 'auto',
+        paddingHorizontal: 0,
+        backgroundColor: Color.indicator.caution[100],
+    },
+    tab: {
+        width: 'auto',
+        padding: 0,
+    },
+    indicator: {
+        backgroundColor: '#000', // Change this to your preferred color
+    },
+};
