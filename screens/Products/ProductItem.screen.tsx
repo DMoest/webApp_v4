30c0d5f075624a2510ed4a9c39f7021cd53e7028
Daniel Andersson <d.andersson86@icloud.com>
2024-06-24 08:36:54 +0200

    Update ProductItem.screen.tsx layout styles

--- screens/Products/ProductItem.screen.tsx
+++ screens/Products/ProductItem.screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {useRoute} from '@react-navigation/native';
-import {Text, View} from 'react-native';
-import {StatusBar} from 'expo-status-bar';
+import { useRoute } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import * as Style from '../../assets/styles';
 
 /**
@@ -9,56 +9,68 @@ import * as Style from '../../assets/styles';
  *
  * @constructor
  */
-export const StockItem: () => JSX.Element = () => {
-    const product = useRoute().params.item;
+export const StockItem: () => React.JSX.Element = () => {
+    const product = useRoute().params?.item;
 
     return (
-        <View style={[Style.Base.content]}>
+        <View style={Style.Container.content}>
             <Text style={Style.Typography.subHeader}>{product.name}</Text>
 
-            <View style={Style.Container.flexBox.row}>
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Produkt id: </Text>
                 <Text style={Style.Typography.dataRight}>{product.id}</Text>
             </View>
 
-            <View style={Style.Container.flexBox.row}>
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Artikel nr: </Text>
                 <Text style={Style.Typography.dataRight}>
                     {product.article_number}
                 </Text>
             </View>
 
-            <View style={Style.Container.flexBox.row}>
-                <Text style={Style.Typography.dataLeft}>Beskrivning: </Text>
-                <Text style={Style.Typography.dataRight}>
-                    {product.description}
-                </Text>
-            </View>
-
-            <View style={Style.Container.flexBox.row}>
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Lagerplats: </Text>
                 <Text style={Style.Typography.dataRight}>
                     {product.location}
                 </Text>
             </View>
 
-            <View style={Style.Container.flexBox.row}>
-                <Text style={Style.Typography.dataLeft}>Antal: </Text>
+            <View style={Style.Container.grid.row}>
+                <Text style={Style.Typography.dataLeft}>Lagersaldo: </Text>
                 <Text style={Style.Typography.dataRight}>
                     {product.stock} st
                 </Text>
             </View>
 
-            <View style={Style.Container.flexBox.row}>
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Pris: </Text>
                 <Text style={Style.Typography.dataRight}>
-                    {product.price} kr / st
+                    {product.price} kr/st
                 </Text>
             </View>
 
-            <View style={Style.Container.flexBox.row}>
+            <View
+                style={[
+                    Style.Container.grid.row,
+                    {
+                        paddingVertical: Style.Typography.whiteSpace[50],
+                    },
+                ]}>
+                <Text style={Style.Typography.dataLeft}>Beskrivning: </Text>
+                <Text style={Style.Typography.dataLeft}>
+                    {product.description}
+                </Text>
+            </View>
+
+            <View
+                style={[
+                    Style.Container.grid.row,
+                    {
+                        paddingVertical: Style.Typography.whiteSpace[50],
+                    },
+                ]}>
                 <Text style={Style.Typography.dataLeft}>Specifikation: </Text>
-                <Text style={Style.Typography.dataRight}>
+                <Text style={Style.Typography.dataLeft}>
                     {product.specifiers}
                 </Text>
             </View>
