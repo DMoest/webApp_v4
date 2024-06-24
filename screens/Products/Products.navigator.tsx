21bc018a4452dd78b6c7dc8a99ca3bee9ee3c8e9
Daniel Andersson <d.andersson86@icloud.com>
2024-06-24 08:41:22 +0200

    Update Products.navigator.tsx component return type and styles

--- screens/Products/Products.navigator.tsx
+++ screens/Products/Products.navigator.tsx
@@ -2,58 +2,54 @@
  * Module imports.
  */
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {NativeStackNavigatorProps} from 'react-native-screens/lib/typescript/native-stack/types';
-import {createStackNavigator} from '@react-navigation/stack';
-import {ProductList} from '../../components/Product/ProductList';
-import {StockItem} from './ProductItem.screen';
-import {StatusBar} from 'expo-status-bar';
-import {CoverImage} from '../../components/Utils/CoverImage';
+import { SafeAreaView, Text, View } from 'react-native';
+import { NativeStackNavigatorProps } from 'react-native-screens/lib/typescript/native-stack/types';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ProductList } from '../../components/Product/ProductList';
+import { StockItem } from './ProductItem.screen';
+import { StatusBar } from 'expo-status-bar';
+import { CoverImage } from '../../components/Utils/CoverImage';
 import coverIMG from '../../assets/img/NutsAndBolts-5.jpg';
 import * as Style from '../../assets/styles';
-
 
 /**
  * Stack navigator for products.
  */
 const Stack: NativeStackNavigatorProps = createStackNavigator();
 
-
 /**
  * Product Stack Navigator.
  *
  * @constructor
  */
-export const ProductsNavigator: () => JSX.Element = () => {
+export const ProductsNavigator: () => React.JSX.Element = () => {
     return (
         <SafeAreaView style={Style.Base.mainContainer}>
-            {CoverImage({headerText: 'Lager', image: coverIMG})}
+            {CoverImage({ headerText: 'Lager', image: coverIMG })}
 
-            <View>
-                <Text
-                    style={[
-                        Style.Typography.paragraph,
-                        Style.Typography.endMarginText,
-                    ]}>
+            <View style={Style.Container.screenIntroductory}>
+                <Text style={Style.Typography.paragraph}>
                     Listan innehåller lagerförda produkter. Varje produkt har
                     ett namn, ett artikelnr. och antal i lager.
                 </Text>
+
+                <Text style={Style.Typography.paragraph}>
+                    Om du klickar på en produkt får du mer information om den.
+                </Text>
             </View>
 
-            <View style={Style.Base.content}>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name='Produkter'
-                        component={ProductList}
-                    />
-                    <Stack.Screen
-                        name='Produktspecifikation'
-                        component={StockItem}
-                    />
-                </Stack.Navigator>
-            </View>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='Produkter'
+                    component={ProductList}
+                />
+                <Stack.Screen
+                    name='Produktspecifikation'
+                    component={StockItem}
+                />
+            </Stack.Navigator>
 
-            <StatusBar style='auto'/>
+            <StatusBar style='auto' />
         </SafeAreaView>
     );
 };
