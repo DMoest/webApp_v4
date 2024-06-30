3f659f4bf01598d94a2ca284bd5111474743ea4c
Daniel Andersson <d.andersson86@icloud.com>
2024-06-30 09:10:25 +0200

    Add interfaces for ProductCreate, ProductUpdate and ProductDelete to Product.ts interfaces module

--- interfaces/Product.ts
+++ interfaces/Product.ts
@@ -1,5 +1,7 @@
 /**
  * Product interface.
+ *
+ * This interface defines the structure of a product object. Used for type checking on requested data.
  */
 export interface Product {
     id: number;
@@ -10,5 +12,54 @@ export interface Product {
     stock: number;
     location: string;
     price: number;
+    api_key: string;
+}
+
+/**
+ * Product create interface.
+ *
+ * This interface defines the structure of a product object used for creating a new product.
+ */
+export interface ProductCreate {
+    // Required fields.
+    api_key: string;
+    name: string;
+
+    // Optional fields.
+    article_number?: string;
+    description?: string;
+    specifiers?: string;
+    stock?: number;
+    location?: string;
+    price?: number;
+}
+
+/**
+ * Product update interface.
+ *
+ * This interface defines the structure of a product object used for updating an existing product.
+ */
+export interface ProductUpdate {
+    // Required fields.
+    id: number;
+    name: string;
+    api_key: string;
+
+    // Optional fields.
+    article_number?: string;
+    description?: string;
+    specifiers?: string;
+    stock?: number;
+    location?: string;
+    price?: number;
+}
+
+/**
+ * Product delete interface.
+ *
+ * This interface defines the structure of a product object used for deleting an existing product.
+ */
+export interface ProductDelete {
+    id: number;
     api_key: string;
 }
