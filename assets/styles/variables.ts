d01b735b8f49807ece1d6cfd5899bbf6a3333b3d
Daniel Andersson <d.andersson86@icloud.com>
2024-07-13 10:50:48 +0200

    Add the variables.ts

---
+++ assets/styles/variables.ts
@@ -0,0 +1,11 @@
+export const borderRadius = {
+    button: 4,
+    card: 10,
+    container: 10,
+    input: 10,
+    modal: 10,
+    section: 10,
+    tab: 10,
+    toast: 10,
+    tooltip: 10,
+};
