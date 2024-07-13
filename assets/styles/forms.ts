be42833e90e827495e78c829a7cf530ef3f3541f
Daniel Andersson <d.andersson86@icloud.com>
2024-07-13 10:46:04 +0200

    Update styles for the baseInput and pickers constants in forms.ts

--- assets/styles/forms.ts
+++ assets/styles/forms.ts
@@ -5,20 +5,36 @@ import * as Color from './colors';
 import * as Container from './containers';
 import * as Typography from './typography';
 
+export const baseInput = {
+    width: '100%',
+    height: 'auto',
+    paddingHorizontal: Typography.whiteSpace[25],
+    paddingVertical: Typography.whiteSpace[50],
+    marginBottom: Typography.whiteSpace[25],
+
+    alignSelf: 'center',
+    justifySelf: 'center',
+
+    // Border
+    borderWidth: 0.5,
+    borderRadius: Container.borderRadius.button,
+    borderColor: Color.grayScale[100],
+
+    // Fonts
+    textAlign: 'left',
+    textAlignVertical: 'middle',
+    fontSize: Typography.fontSize.text,
+    fontFamily: Typography.fontFamily.text,
+    color: Color.text.dark,
+    backgroundColor: Color.background.light,
+};
+
 /**
  *
  */
 export const textInputField = {
-    width: '100%',
-    height: 'auto',
-    paddingHorizontal: Typography.whiteSpace[50],
-    paddingVertical: Typography.whiteSpace[50],
-    marginBottom: Typography.whiteSpace[10],
-    backgroundColor: Color.grayScale.gray4,
-    color: Color.text.dark,
-    borderRadius: Container.borderRadius.button,
-    justifySelf: 'center',
-    alignSelf: 'center',
+    ...baseInput,
+    backgroundColor: Color.schemeOne.primary[100],
 };
 
 export const labelInputField = {
@@ -30,9 +46,11 @@ export const labelInputField = {
 
 export const pickers = {
     width: '100%',
-    // maxHeight: 20,
+    maxHeight: 10,
     paddingHorizontal: 0,
     paddingVertical: 0,
+    marginVertical: 0,
+    marginHorizontal: 0,
 };
 
 // export const pickerItems = {
