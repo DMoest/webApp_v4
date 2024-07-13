df810da1e535b277bdca602ef4e1e9ff85a846a0
Daniel Andersson <d.andersson86@icloud.com>
2024-07-13 10:19:14 +0200

    Structure the template part better in DeliveryItem.screen.tsx and fix some Styles

--- screens/Deliveries/DeliveryItem.screen.tsx
+++ screens/Deliveries/DeliveryItem.screen.tsx
@@ -21,29 +21,41 @@ export const DeliveryItem: (
 
     return (
         <View style={Style.Container.content}>
-            <View style={Style.Container.flexBox.rowNoPadding}>
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Inleverans ID: </Text>
                 <Text style={Style.Typography.dataRight}>{item.id}</Text>
             </View>
-            <View style={Style.Container.flexBox.rowNoPadding}>
+
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Produkt ID: </Text>
                 <Text style={Style.Typography.dataRight}>
                     {item.product_id}
                 </Text>
             </View>
-            <View style={Style.Container.flexBox.rowNoPadding}>
+
+            <View style={Style.Container.grid.row}>
                 <Text style={Style.Typography.dataLeft}>Produktnamn: </Text>
                 <Text style={Style.Typography.dataRight}>
                     {item.product_name}
                 </Text>
             </View>
+
             <View style={Style.Container.flexBox.rowNoPadding}>
                 <Text style={Style.Typography.dataLeft}>Antal: </Text>
                 <Text style={Style.Typography.dataRight}>{item.amount} st</Text>
             </View>
 
-            <Text style={Style.Typography.dataLeft}>Kommentar: </Text>
-            <Text style={Style.Typography.dataLeft}>{item.comment}</Text>
+            <View
+                style={
+                    (Style.Container.grid.column,
+                    {
+                        paddingTop: Typography.whiteSpace[100],
+                        paddingBottom: Typography.whiteSpace[200],
+                    })
+                }>
+                <Text style={Style.Typography.dataLeft}>Kommentar: </Text>
+                <Text style={Style.Typography.dataLeft}>{item.comment}</Text>
+            </View>
 
             <StatusBar style='auto' />
         </View>
