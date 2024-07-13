08dd32d81d6dd99653908407b5a4b231b2e14aaa
Daniel Andersson <d.andersson86@icloud.com>
2024-07-13 10:59:49 +0200

    Update imports, styles used and return type for the component Home.screen.tsx

--- screens/Home.screen.tsx
+++ screens/Home.screen.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
-// eslint-disable-next-line import/namespace
 import {SafeAreaView, Text, View} from 'react-native';
 import {CoverImage} from '../components/Utils/CoverImage';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import coverIMG from '../assets/img/NutsAndBolts-4.jpg';
 import * as Style from '../assets/styles';
+
 
 /**
  * Home screen/view.
  *
  * @constructor
  */
-export const Home: React.FC = () => {
+export const Home: React.FC = (): React.JSX.Element => {
     return (
         <SafeAreaView style={Style.Base.mainContainer}>
-            {CoverImage({ headerText: 'Infinity', image: coverIMG })}
+            {CoverImage({headerText: 'Infinity', image: coverIMG})}
 
-            <View
-                style={[
-                    Style.Container.containers,
-                    { paddingHorizontal: Style.Typography.whiteSpace.X075 },
-                ]}>
+            <View style={Style.Container.content}>
                 <Text style={Style.Typography.paragraph}>
                     Välkommen till vår fiktiva mobila lagerapp. Idag lagrar vi
                     skruv och skrot, imorgon kanske något helt annat och den som
                     lever då får se. Hoppas ni trivs med att använda vår app och
-                    hittar något riktigt rostigt.{' '}
+                    hittar något riktigt rostigt i vårt lager.
                 </Text>
                 <Text
                     style={[
