7373e6876ee4f5cf20955b1daf913c9424a5be1b
Daniel Andersson <d.andersson86@icloud.com>
2024-07-13 11:20:30 +0200

    Update Invoices.tsx models module, add docstrings and lint

--- models/Invoices.tsx
+++ models/Invoices.tsx
@@ -1,73 +1,168 @@
-import React from 'react';
-import {RequestErrorHandler} from '../components/Utils/ErrorHandler';
+import axios, {AxiosResponse} from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import config from '../config/config.json';
 import * as InvoiceInterfaces from '../interfaces/Invoice';
-import {NewInvoice} from "../interfaces/Invoice";
+import {RequestErrorHandler} from '../components/Utils/ErrorHandler';
+
 
 /**
- * Getter Model Method for getting all available invoices from the API.
+ * Fetches all invoices from the API.
+ *
+ * This function retrieves a JWT token from SecureStore and uses it to authenticate a GET request to the API.
+ * The request fetches all invoices and logs the response data. If an error occurs during the request, it is caught,
+ * logged, and passed to the RequestErrorHandler function. In the case of an error, the function returns an empty
+ * array.
+ *
+ * @returns {Promise<InvoiceInterfaces.Invoice[]>} The response data from the API, which contains the invoices, or
+ * an empty array if an error occurs.
+ * @throws {Error} If there is an error during the request, it is caught, logged, and passed to the
+ * RequestErrorHandler function.
  */
-export async function getInvoices(): Promise<InvoiceInterfaces.Invoice[]> {
+export async function getInvoices() {
     try {
-        // Get token from SecureStore.
-        const jwtToken = await SecureStore.getItemAsync('token');
+        console.log('getInvoices()');
 
-        // Fetch invoices from API.
-        const response = await fetch(
+        const jwtToken: string | null = await SecureStore.getItemAsync('token');
+        const response = await axios.get(
             `${config.base_url}/invoices?api_key=${config.api_key}`,
             {
-                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'x-access-token': jwtToken,
+                    'Content-Type': 'application/json',
+                },
+                timeout: 3000,
+            },
+        );
+
+        if (response.data.data) {
+            return response.data;
+        }
+
+        return response;
+    } catch (error) {
+        RequestErrorHandler(error);
+        return [];
+    }
+}
+
+/**
+ * Fetches a specific invoice from the API by its ID.
+ *
+ * This function retrieves a JWT token from SecureStore and uses it to authenticate a GET request to the API.
+ * The request fetches a specific invoice by its ID and returns the response data. If an error occurs during the request, it is caught,
+ * logged, and passed to the RequestErrorHandler function. In the case of an error, the function returns null.
+ *
+ * @param {number} invoice_id - The ID of the invoice to fetch.
+ * @returns {Promise<InvoiceInterfaces.Invoice | null>} The response data from the API, which contains the invoice, or null if an error occurs.
+ * @throws {Error} If there is an error during the request, it is caught, logged, and passed to the RequestErrorHandler function.
+ */
+export async function getInvoiceById(
+    invoice_id: number,
+): Promise<InvoiceInterfaces.Invoice | null> {
+    try {
+        const jwtToken = await SecureStore.getItemAsync('token');
+        const response = await axios.get(
+            `${config.base_url}/invoices/${invoice_id}?api_key=${config.api_key}`,
+            {
                 headers: {
                     Accept: 'application/json',
                     'x-access-token': jwtToken,
                 },
+                timeout: 3000,
             },
         );
 
-        // Await JSON response.
-        const result = await response.json();
-
-        console.log('Invoices fetched from API: ', result.data);
-        return result.data;
+        return response.data;
     } catch (error) {
-        console.error(error);
         RequestErrorHandler(error);
+        return null;
     }
 }
 
-export async function createInvoice(invoice: Partial<NewInvoice>) {
+/**
+ * Creates a new invoice in the API.
+ *
+ * This function retrieves a JWT token from SecureStore and uses it to authenticate a POST request to the API.
+ * The request creates a new invoice with the provided invoice details and returns the response data. If an error
+ * occurs during the request, it is caught, logged, and passed to the RequestErrorHandler function. In the case of
+ * an error, the function returns null.
+ *
+ * @param {Partial<InvoiceInterfaces.NewInvoice>} invoice - The details of the invoice to create.
+ * @returns {Promise<InvoiceInterfaces.Invoice | null>} The response data from the API, which contains the created
+ *  invoice, or null if an error occurs.
+ * @throws {Error} If there is an error during the request, it is caught, logged, and passed to the
+ *  RequestErrorHandler function.
+ */
+export async function createInvoice(
+    invoice: Partial<InvoiceInterfaces.NewInvoice>,
+): Promise<InvoiceInterfaces.Invoice | null> {
     try {
-        const jwtToken = await SecureStore.getItemAsync('token');
+        const jwtToken: string | null = await SecureStore.getItemAsync('token');
+        const response: AxiosResponse<InvoiceInterfaces.Invoice> =
+            await axios.post(
+                `${config.base_url}/invoices`,
+                {
+                    order_id: invoice.order_id,
+                    total_price: invoice.total_price,
+                    creation_date: invoice.creation_date,
+                    due_date: invoice.due_date,
+                    api_key: config.api_key,
+                },
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        'x-access-token': jwtToken,
+                    },
+                    timeout: 3000,
+                },
+            );
 
-        console.log(
-            'InvoiceModel.createInvoice() ~> Invoice to be created: ',
-            invoice,
-        );
-
-        return await fetch(`${config.base_url}/invoices`, {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                'x-access-token': jwtToken,
-            },
-            body: JSON.stringify({
-                order_id: invoice.order_id,
-                total_price: invoice.total_price,
-                creation_date: invoice.creation_date,
-                due_date: invoice.due_date,
-                api_key: config.api_key,
-            }),
-        });
+        return response.data;
     } catch (error) {
-        console.error(error);
         RequestErrorHandler(error);
+        return null;
     }
 }
 
-export async function getInvoiceById(
-    invoice_id: number,
-): Promise<InvoiceInterfaces.Invoice[]> {
-    console.log('Get invoice by ID: ', invoice_id);
+/**
+ * Updates an existing invoice in the API.
+ *
+ * This function retrieves a JWT token from SecureStore and uses it to authenticate a PUT request to the API.
+ * The request updates an existing invoice with the provided invoice details and returns the status of the response.
+ * If an error occurs during the request, it is caught, logged, and passed to the RequestErrorHandler function.
+ * In the case of an error, the function returns null.
+ *
+ * @param {Partial<InvoiceInterfaces.Invoice>} updated_invoice - The updated details of the invoice.
+ * @returns {Promise<number | null>} The status of the response from the API, or null if an error occurs.
+ * @throws {Error} If there is an error during the request, it is caught, logged, and passed to the
+ * RequestErrorHandler function.
+ */
+export async function updateInvoice(
+    updated_invoice: Partial<InvoiceInterfaces.Invoice>,
+): Promise<number | null> {
+    try {
+        const jwtToken: string | null = await SecureStore.getItemAsync('token');
+        const response: AxiosResponse = await axios.put(
+            `${config.base_url}/invoices/${updated_invoice.id}`,
+            {
+                ...updated_invoice,
+                api_key: config.api_key,
+            },
+            {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'x-access-token': jwtToken,
+                },
+                timeout: 3000,
+            },
+        );
+
+        return response.status;
+    } catch (error) {
+        RequestErrorHandler(error);
+        return null;
+    }
 }
