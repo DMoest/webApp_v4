793a4ec7a4decdb65b53ec3f8a074b0f0c66d192
Daniel Andersson <d.andersson86@icloud.com>
2024-07-14 14:45:48 +0200

    Add docstrings to explain function/method/hook/etc.

--- components/Delivery/DeliveryList.tsx
+++ components/Delivery/DeliveryList.tsx
@@ -8,6 +8,7 @@ import * as DeliveriesInterfaces from '../../interfaces/Deliveries';
 import * as DeliveryModel from '../../models/Deliveries';
 import * as Style from '../../assets/styles';
 
+
 /**
  * DeliveryList object to fetch item list from API and generate a FlatList View from response JSON object.
  *
@@ -18,21 +19,18 @@ export const DeliveryList: React.FC = ({route}): React.JSX.Element => {
     const navigation = useNavigation();
     let reload = route.params?.reload ?? true;
 
+
     /**
-     * React Hook to load deliveries and products.
+     * Asynchronously loads deliveries from the DeliveryModel and updates the app context with the fetched
+     * deliveries. It sets the app context's `isRefreshing` state to true at the beginning of the operation and to
+     * false upon completion or failure. If the operation is successful, the fetched deliveries are stored in the app
+     * context's `deliveries` state. In case of an error, the error is logged to the console. Regardless of the
+     * outcome, the `reload` flag is set to false at the end of the operation to indicate that a reload is no
+     * longer necessary.
+     *
+     * @returns {Promise<DeliveriesInterfaces.Deliveries[] | undefined>} A promise that resolves to the list of
+     * fetched deliveries or undefined if an error occurs.
      */
-    useEffect(() => {
-        appContext.setIsRefreshing(true);
-
-        void loadDeliveries().then((): void => {
-            // Set RELOAD to false.
-            reload = false;
-
-            // Set isRefreshing to false.
-            appContext.setIsRefreshing(false);
-        });
-    }, [reload]);
-
     async function loadDeliveries() {
         appContext.setIsRefreshing(true);
 
@@ -49,9 +47,14 @@ export const DeliveryList: React.FC = ({route}): React.JSX.Element => {
     }
 
     /**
-     * Renders Delivery item in a FlatList.
+     * Renders a single delivery item as a pressable component within a list.
      *
-     * @param item
+     * This component changes its background color when pressed, providing visual feedback to the user.
+     * Upon pressing, it navigates to the 'Inleveransspecifikation' screen with the item's details.
+     *
+     * @param {Object} props - The properties passed to the renderItem function.
+     * @param {Object} props.item - The delivery item data to be rendered.
+     * @returns {React.ReactElement} A pressable component representing a single delivery item.
      */
     const renderItem = ({item}) => (
         <Pressable
@@ -71,6 +74,22 @@ export const DeliveryList: React.FC = ({route}): React.JSX.Element => {
         </Pressable>
     );
 
+
+    /**
+     * Memoizes and returns a React JSX element that conditionally renders either a message indicating
+     * there are no deliveries or a FlatList of delivery items based on the current state of deliveries in the app
+     * context.
+     *
+     * If there are no deliveries (`appContext.deliveries.length === 0`), it renders a view with a warning message.
+     * Otherwise, it renders a FlatList component populated with delivery items. The FlatList is configured to
+     * refresh on pull-down, invoking the `loadDeliveries` function to fetch and display the latest deliveries.
+     *
+     * The memoization is dependent on `appContext.deliveries` and `appContext.isRefreshing`, ensuring that the
+     * component only re-renders when these values change, for performance optimization.
+     *
+     * @returns {React.JSX.Element} A React JSX element that is either a message indicating no deliveries or
+     * a FlatList of delivery items.
+     */
     const renderDeliveriesList: React.JSX.Element = useMemo(() => {
         if (appContext.deliveries.length === 0) {
             return (
