5824d70e25b35f7483b9cb544ade36ab2c8414cd
Daniel Andersson <d.andersson86@icloud.com>
2024-07-17 20:15:07 +0200

    Fix type lint error for ImageBackground component usage in CoverImage.tsx by adding new type from ImageSourcePropType in UtilsInterfaces

--- components/Utils/CoverImage.tsx
+++ components/Utils/CoverImage.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
-import { ScreenCoverArguments } from '../../interfaces/Utils';
+import {ImageBackground, ImageSourcePropType, Text, View} from 'react-native';
+import {ScreenCoverArguments} from '../../interfaces/Utils';
 import * as Style from '../../assets/styles';
 
+
 /**
- * Screen cover image component.
+ * Renders a cover image with an overlay text.
  *
- * @param coverIMG
- * @param headerText
- * @constructor
+ * This component displays an image as the background of a view, with a text header overlaid on top.
+ * It supports both local and remote images by dynamically setting the image source based on the
+ * type of the `image` prop. The component is styled using predefined styles from the `Style` module.
+ *
+ * @param {ScreenCoverArguments} props The component props.
+ * @param {string | ImageSourcePropType} props.image The source of the image. Can be a URI string for
+ * remote images or an ImageSourcePropType object for local images.
+ * @param {string} props.headerText The text to display on top of the image.
+ * @returns {React.JSX.Element} A React component representing the cover image with text.
  */
 export function CoverImage({
     image,
     headerText,
 }: ScreenCoverArguments): React.JSX.Element {
+    const imageSource: ImageSourcePropType = typeof image === 'string' ? {uri: image} : image;
+
+
     return (
         <View style={Style.Image.imageContainer}>
             <ImageBackground
-                source={image}
+                source={imageSource}
                 style={Style.Image.image}>
-                <Text style={Style.Typography.header}>{headerText}</Text>
+                <Text style={Style.Typography.header}>
+                    {headerText}
+                </Text>
             </ImageBackground>
         </View>
     );
--- interfaces/Utils.ts
+++ interfaces/Utils.ts
@@ -1,7 +1,28 @@
 /**
- * Interface for the arguments of the screen cover element component.
+ * Defines the properties for the ScreenCover component.
+ *
+ * @interface ScreenCoverArguments
+ * @property {string} image - The source URI of the image to be displayed as the cover. Can be a local or
+ * remote URL.
+ * @property {string} headerText - The text to be displayed over the image as a header.
  */
 export interface ScreenCoverArguments {
     image: string;
     headerText: string;
+}
+
+
+/**
+ * Describes the properties for specifying an image source in components.
+ *
+ * This interface is used to define the source of an image, supporting both local and remote images.
+ * - `image`: A string specifying the path to a local image when using a local source.
+ * - `headerText`: A string intended for accompanying text or description, not directly related to the
+ * image source itself.
+ * - `uri`: An optional string for specifying the URL of a remote image.
+ */
+export interface ImageSourcePropType {
+    headerText: string;
+    image: string;
+    uri?: string;
 }
