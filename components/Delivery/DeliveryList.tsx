f9908231824aa4d00b46982749e949597d65f468
Daniel Andersson <d.andersson86@icloud.com>
2024-07-17 20:51:29 +0200

    Add docstrings to explain function/method/hook/etc. in DeliveryList.tsx

--- components/Delivery/DeliveryList.tsx
+++ components/Delivery/DeliveryList.tsx
@@ -10,9 +10,34 @@ import * as Style from '../../assets/styles';
 
 
 /**
- * DeliveryList object to fetch item list from API and generate a FlatList View from response JSON object.
+ * `DeliveryList` is a functional component that manages the display of a list of deliveries. It
+ * integrates with the application's context to fetch and display delivery data, leveraging the React
+ * Navigation library for navigation and the use of focus effects to reload data as needed. This component
+ * is responsible for fetching delivery and product data asynchronously, handling user interactions to
+ * navigate to detailed views, and providing visual feedback during data loading states.
  *
- * @constructor
+ * The component performs the following key functions:
+ * - Fetches delivery and product data asynchronously from a model and updates the application context
+ *   with this data.
+ * - Utilizes the `useFocusEffect` hook to reload data when the component gains focus, based on specific
+ *   conditions
+ *   such as the `reload` flag being true or the deliveries list being empty.
+ * - Renders a list of deliveries using the `FlatList` component from React Native, with each item in the
+ *   list being
+ *   pressable to navigate to a detailed view of the delivery.
+ * - Provides a visual indication of loading state and the ability to refresh the deliveries list via a
+ *   pull-down gesture.
+ * - Offers a button to navigate to a form for creating a new delivery.
+ *
+ * Props: None
+ *
+ * State:
+ * - Uses context state for managing deliveries, products, and the refreshing state.
+ * - Local state `reload` is derived from navigation parameters to determine if data should be reloaded.
+ *
+ * Returns:
+ * - A React element that conditionally renders either a loading indicator or the list of deliveries,
+ *   along with a button for creating new deliveries.
  */
 export const DeliveryList: React.FC = (): React.ReactElement => {
     const appContext = useAppContext();
@@ -23,14 +48,14 @@ export const DeliveryList: React.FC = (): React.ReactElement => {
 
     /**
      * Asynchronously loads deliveries from the DeliveryModel and updates the app context with the fetched
-     * deliveries. It sets the app context's `isRefreshing` state to true at the beginning of the operation and to
-     * false upon completion or failure. If the operation is successful, the fetched deliveries are stored in the app
-     * context's `deliveries` state. In case of an error, the error is logged to the console. Regardless of the
-     * outcome, the `reload` flag is set to false at the end of the operation to indicate that a reload is no
-     * longer necessary.
+     * deliveries. It sets the app context's `isRefreshing` state to true at the beginning of the
+     * operation and to false upon completion or failure. If the operation is successful, the fetched
+     * deliveries are stored in the app context's `deliveries` state. In case of an error, the error is
+     * logged to the console. Regardless of the outcome, the `reload` flag is set to false at the end of
+     * the operation to indicate that a reload is no longer necessary.
      *
-     * @returns {Promise<DeliveriesInterfaces.Deliveries[] | undefined>} A promise that resolves to the list of
-     * fetched deliveries or undefined if an error occurs.
+     * @returns {Promise<DeliveriesInterfaces.Deliveries[] | undefined>} A promise that resolves to the
+     * list of fetched deliveries or undefined if an error occurs.
      */
     async function loadDeliveries() {
         appContext.setIsRefreshing(true);
@@ -49,14 +74,15 @@ export const DeliveryList: React.FC = (): React.ReactElement => {
 
 
     /**
-     * Triggers the `loadDeliveries` function when the component gains focus and either the `reload` flag is set to
-     * true or there are no orders in the app context. This effect is designed to ensure that the deliveries list is
-     * up-to-date whenever the user navigates to the component. After successfully loading the deliveries, it resets
-     * the `reload` parameter to false to prevent unnecessary reloads on subsequent focus events.
+     * Triggers the `loadDeliveries` function when the component gains focus and either the `reload` flag
+     * is set to true or there are no orders in the app context. This effect is designed to ensure that
+     * the deliveries list is up-to-date whenever the user navigates to the component. After successfully
+     * loading the deliveries, it resets the `reload` parameter to false to prevent unnecessary reloads on
+     * subsequent focus events.
      *
-     * The effect is dependent on the `reload` flag, the length of the deliveries array in the app context, and the
-     * `setParams` method from the navigation object, meaning it will re-run only when any of these dependencies
-     * change.
+     * The effect is dependent on the `reload` flag, the length of the deliveries array in the app
+     * context, and the `setParams` method from the navigation object, meaning it will re-run only when
+     * any of these dependencies change.
      */
     useFocusEffect(
         useCallback((): void => {
@@ -101,15 +127,16 @@ export const DeliveryList: React.FC = (): React.ReactElement => {
 
     /**
      * Memorizes and returns a React JSX element that conditionally renders either a message indicating
-     * there are no deliveries or a FlatList of delivery items based on the current state of deliveries in the app
-     * context.
+     * there are no deliveries or a FlatList of delivery items based on the current state of deliveries
+     * in the app context.
      *
-     * If there are no deliveries (`appContext.deliveries.length === 0`), it renders a view with a warning message.
-     * Otherwise, it renders a FlatList component populated with delivery items. The FlatList is configured to
-     * refresh on pull-down, invoking the `loadDeliveries` function to fetch and display the latest deliveries.
+     * If there are no deliveries (`appContext.deliveries.length === 0`), it renders a view with a
+     * warning message. Otherwise, it renders a FlatList component populated with delivery items. The
+     * FlatList is configured to refresh on pull-down, invoking the `loadDeliveries` function to fetch and
+     * display the latest deliveries.
      *
-     * The memoization is dependent on `appContext.deliveries` and `appContext.isRefreshing`, ensuring that the
-     * component only re-renders when these values change, for performance optimization.
+     * The memoization is dependent on `appContext.deliveries` and `appContext.isRefreshing`, ensuring
+     * that the component only re-renders when these values change, for performance optimization.
      *
      * @returns {React.JSX.Element} A React JSX element that is either a message indicating no deliveries or
      * a FlatList of delivery items.
