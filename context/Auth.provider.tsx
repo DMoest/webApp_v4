ae0dca6de1cee8124724ae7aa4c88242be0367ee
Daniel Andersson <d.andersson86@icloud.com>
2024-07-17 21:21:34 +0200

    Update Auth.provider.tsx component

--- context/Auth.provider.tsx
+++ context/Auth.provider.tsx
@@ -1,48 +1,63 @@
+/**
+ * Provides a React context for authentication, encapsulating the logic for user login, logout, and registration.
+ * This context makes user authentication state and functions available throughout the component tree.
+ *
+ * The `AuthProvider` component wraps the application's component tree to provide every child component access to the authentication context.
+ * It maintains the state of the current user, whether the user is logged in, and any authentication-related loading or error states.
+ * Functions for login, logout, and registration are provided to manipulate the authentication state.
+ *
+ * @module AuthProvider
+ * @exports AuthProvider The provider component for the authentication context.
+ * @exports useAuthContext A hook to access the authentication context.
+ */
 import React, {createContext, useState} from 'react';
 import * as AuthInterfaces from '../interfaces/Auth';
 import * as AuthModel from '../models/Auth';
 import * as SecureStore from 'expo-secure-store';
+import {AuthContextType} from "../interfaces/Auth";
 
-
-/**
- * Authentication context types.
- */
-type AuthContextType = {
-    user: AuthInterfaces.User | undefined;
-    setUser: (user: AuthInterfaces.User) => void;
-    isLoggedIn: boolean;
-    setIsLoggedIn: (authIndicator: boolean) => void;
-    login: (username: string, password: string) => Promise<void>;
-    logout: () => Promise<void>; // Note the Promise<void> since logout is async
-    register: (username: string, password: string) => Promise<void>; // Note the Promise<void> since register is async
-};
 
 /**
  * Authentication context.
+ *
+ * This context initializes with default values and provides types for the authentication-related state
+ * and functions. It includes the current user, login state, and functions for login, logout, and
+ * registration.
+ *
+ * @context
+ * @type {React.Context<AuthContextType>}
  */
-const AuthContext = createContext<AuthContextType>({
+const AuthContext: React.Context<AuthInterfaces.AuthContextType> = createContext<AuthContextType>({
     user: undefined,
     setUser(user: AuthInterfaces.User | null): void {
     },
     isLoggedIn: false,
-    setIsLoggedIn: () => {
+    setIsLoggedIn: (): void => {
     },
-    login: async (email: string, password: string) => {
+    login: async (email: string, password: string): Promise<void> => {
         await AuthModel.login(email, password);
     },
-    logout: async () => {
+    logout: async (): Promise<void> => {
         await AuthModel.logout();
     },
-    register: async (email: string, password: string) => {
+    register: async (email: string, password: string): Promise<void> => {
         await AuthModel.register(email, password);
     },
 });
 
+
 /**
  * Authentication context provider.
  *
- * @param children
- * @constructor
+ * This component manages the authentication state, including the current user, login status, and any
+ * authentication errors. It provides functions to login, logout, and register, which update the
+ * authentication state accordingly. Children components can access the authentication state and functions
+ * through the `useAuthContext` hook.
+ *
+ * @param {React.ReactNode} children - The child components that will have access to the authentication
+ * context.
+ * @returns {React.JSX.Element} The provider component wrapping its children, providing them access to
+ * the authentication context.
  */
 export const AuthProvider: React.FC = ({children}) => {
     const [user, setUser] = useState<AuthInterfaces.User | null>(null);
@@ -50,14 +65,15 @@ export const AuthProvider: React.FC = ({children}) => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
     const [error, setError] = useState<Error | null>(null);
 
-    const login = async (email: string, password: string) => {
+
+    const login = async (email: string, password: string): Promise<void> => {
         setIsLoading(true);
 
         try {
             // Assuming AuthModel.login() sets some kind of global state or does something else
             // that makes the user "logged in" from the system's perspective.
             await AuthModel.login(email, password).then((user) => {
-                console.log('AuthProvider -> login -> user\n', user);
+                console.info('AuthProvider -> login -> user\n', user);
                 setUser(user);
 
                 return setIsLoggedIn(true);
@@ -72,7 +88,7 @@ export const AuthProvider: React.FC = ({children}) => {
         }
     };
 
-    const logout = async () => {
+    const logout = async (): Promise<void> => {
         try {
             await SecureStore.deleteItemAsync('token');
             setUser(null);
@@ -82,7 +98,7 @@ export const AuthProvider: React.FC = ({children}) => {
         }
     };
 
-    const register = async (email: string, password: string) => {
+    const register = async (email: string, password: string): Promise<void> => {
         setIsLoading(true);
 
         try {
@@ -116,6 +132,13 @@ export const AuthProvider: React.FC = ({children}) => {
 
 
 /**
- * Auth context access function export.
+ * useAuthContext hook.
+ *
+ * A custom hook that provides access to the authentication context.
+ * Components using this hook can access the current user, authentication state, and functions for login,
+ * logout, and registration.
+ *
+ * @returns {AuthContextType} The authentication context, including the current user, authentication
+ * state, and related functions.
  */
-export const useAuthContext = () => React.useContext(AuthContext);
+export const useAuthContext = (): AuthContextType => React.useContext(AuthContext);
