d32a41fdb50a6db617daefe56a6873b1b8a58795
Daniel Andersson <d.andersson86@icloud.com>
2024-07-31 11:46:12 +0200

    Remove unused constant in colors.ts

--- assets/styles/colors.ts
+++ assets/styles/colors.ts
@@ -1,44 +1,4 @@
 /**
- * All Standard Colors.
- */
-export const colors = {
-    // Colors
-    primaryColor: '#A4CFBE',
-    secondaryColor: '#417378',
-    tertiaryColor: '#f4f7d9',
-    quarternaryColor: '#6c6e58',
-    quintaryColor: '#3e423a',
-
-    // Text Colors
-    textColorDark: '#1c1c1c',
-    textColorLight: '#fafafa',
-
-    // Background Colors
-    backgroundColor: '#ffffff',
-    bgLight: '#ffffff',
-    bgDark: '#2a2a2a',
-
-    // Shadows
-    shadows: '#3F3F3F',
-
-    // B&W + Grayscale
-    black: '#090909',
-    gray1: '#3F3F3F',
-    gray2: '#8C8C8C',
-    gray3: '#BFBFBF',
-    gray4: '#F2F2F2',
-    white: '#ffffff',
-
-    // Indicator colors
-    positiveColor: '#00A750',
-    cautionColor: '#FFD702',
-    cautionColor2: '#FFA500',
-    warningColor: '#ED1C24',
-    linkColor: '#104AA1',
-    linkColor2: '#662C91',
-};
-
-/**
  * Color Scheme One.
  */
 export const schemeOne = {
